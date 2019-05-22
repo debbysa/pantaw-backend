5397a8d88a68c9c0d4a411947ce81bab6991cf75
debbysa <deby.silvia23@gmail.com>
2019-05-22 22:56:30 +0700

    menambahkan jwt verify untuk menampilkan nama pengirim

--- controller/PercakapanController.js
+++ controller/PercakapanController.js
@@ -1,12 +1,21 @@
 const Percakapan = require("../model/Percakapan");
-const Workshop = require("../model/Workshop");
+const Pemateri = require("../model/Pemateri");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   //data percakapan workshop
   index: function(req, res) {
-    Percakapan.findAll({ where: { id_workshop: req.params.id } }).then(function(
-      rows
-    ) {
+    Percakapan.findAll({
+      where: { id_workshop: req.params.id },
+      include: { model: Pemateri },
+      order: [["id_percakapan", "ASC"]]
+    }).then(function(rows) {
+      rows.forEach(row => {
+        if (row.pemateri == null)
+          row.set("peserta", {
+            nama: jwt.verify(row.id_pengirim, "randomStuff")
+          });
+      });
       res.json(rows);
     });
   },
