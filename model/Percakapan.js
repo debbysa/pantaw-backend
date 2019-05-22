59b982bc0c85780d54da26dabf8873486a5c879a
debbysa <deby.silvia23@gmail.com>
2019-05-22 22:58:59 +0700

    menambahkan field peserta dengan tipe virtual, join percakapan dengan table pemateri

--- model/Percakapan.js
+++ model/Percakapan.js
@@ -1,32 +1,37 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../config/db')
-const Workshop = require('./Workshop')
+const Sequelize = require("sequelize");
+const sequelize = require("../config/db");
+const Pemateri = require("./Pemateri");
 
 const Percakapan = sequelize.define(
-    "percakapan",
-    {
-        id_percakapan: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        id_workshop: {
-            type: Sequelize.INTEGER
-        },
-        id_pengirim: {
-            type: Sequelize.INTEGER
-        },
-        pesan: {
-            type: Sequelize.TEXT
-        },
-        tanggal_waktu: {
-            type: Sequelize.DATE
-        }
+  "percakapan",
+  {
+    id_percakapan: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
-    { timestamps: false, tableName: "percakapan" }
+    id_workshop: {
+      type: Sequelize.INTEGER
+    },
+    id_pengirim: {
+      type: Sequelize.INTEGER
+    },
+    peserta: {
+      type: Sequelize.VIRTUAL
+    },
+    pesan: {
+      type: Sequelize.TEXT
+    },
+    tanggal_waktu: {
+      type: Sequelize.DATE
+    }
+  },
+  { timestamps: false, tableName: "percakapan" }
 );
 
 // Percakapan.belongsTo(Workshop, { foreignKey: 'id_workshop' })
 // Detail_workshop.belongsTo(Peserta, { foreignKey: 'id_peserta' })
 
-module.exports = Percakapan
+Percakapan.belongsTo(Pemateri, { foreignKey: "id_pengirim" });
+
+module.exports = Percakapan;
