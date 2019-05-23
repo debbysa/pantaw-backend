5a9c457a68d914f25148dd27b3b88abc1e4de6c5
debbysa <deby.silvia23@gmail.com>
2019-05-23 11:51:37 +0700

    mengatur autentikasi peserta

--- controller/AutentikasiController.js
+++ controller/AutentikasiController.js
@@ -22,22 +22,22 @@ module.exports = {
   authenticatePeserta: function(req, res) {
     const { nama, id_workshop } = req.body;
     jwt.sign(nama, "randomStuff", function(err, token) {
+      if (err) res.json({ success: false, error: err });
+
       DetailWorkshop.create({ id_peserta: token, id_workshop }).then(function(
         row
       ) {
-        res.json({ success: true, token });
+        row.set("peserta", { nama: jwt.verify(row.id_peserta, "randomStuff") });
+        res.json({ success: true, token, detail_workshop: row });
       });
     });
   },
 
   deauthenticatePeserta: function(req, res) {
-    const { id_workshop, id_peserta } = req.body;
-    DetailWorkshop.destroy({
-      where: {
-        id_workshop,
-        id_peserta
-      }
-    }).then(function(rows) {
+    const { id_detail_workshop } = req.body;
+    console.log(id_detail_workshop);
+    DetailWorkshop.findByPk(id_detail_workshop).then(function(row) {
+      row.destroy();
       res.json({ success: true });
     });
   },
